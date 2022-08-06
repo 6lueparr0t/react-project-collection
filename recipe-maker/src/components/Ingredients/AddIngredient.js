1549a0cfdff2f4adc1c24e73ea0fb56f920c62db
6lueparr0t <6lueparr0t@gmail.com>
2022-08-06 15:26:13 +0900

    update useeffect-hook

--- recipe-maker/src/components/Ingredients/AddIngredient.js
+++ recipe-maker/src/components/Ingredients/AddIngredient.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
@@ -7,15 +7,25 @@ import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddIngredient.module.scss";
 
 const AddIngredient = (props) => {
-  const [enteredIngrdientName, setEnteredIngrdientName] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
+  const ingredientInputRef = useRef();
+  const amountInputRef = useRef();
+
+  // 이제 useRef 로 관리한다.
+  // useRef 를 사용하면 제어되지 않는 컴포넌트가 된다. 반면 useState 를 사용한 방식은 제어된 컴포넌트 다.
+  // const [enteredIngredientName, setEnteredIngrdientName] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState("");
   const [error, setError] = useState("");
 
   const addIngredientHandler = (event) => {
     event.preventDefault();
+    // console.log(ingredientInputRef.current.value, amountInputRef.current.value);
+
+    const enteredIngredientNameRef = ingredientInputRef.current.value;
+    const enteredAmountRef = amountInputRef.current.value;
+
     if (
-      enteredIngrdientName.trim().length === 0 ||
-      enteredAmount.trim().length === 0
+      enteredIngredientNameRef.trim().length === 0 ||
+      enteredAmountRef.trim().length === 0
     ) {
       return setError({
         title: "재료가 비어있어요!",
@@ -23,33 +33,37 @@ const AddIngredient = (props) => {
       });
     }
 
-    if (+enteredAmount < 1) {
+    if (+enteredAmountRef < 1) {
       return setError({
         title: "너무 적어요ㅠ",
         message: "1 이상의 숫자만 넣어주세요!",
       });
     }
 
-    if (+enteredAmount > 3) {
+    if (+enteredAmountRef > 3) {
       return setError({
         title: "너무 많아요~ 🐷",
         message: "3 이하의 숫자만 넣어주세요~",
       });
     }
 
-    props.onAddIngredient(enteredIngrdientName, enteredAmount);
-    // console.log(enteredIngrdientName, enteredAmount);
-    setEnteredIngrdientName("");
-    setEnteredAmount("");
+    props.onAddIngredient(enteredIngredientNameRef, enteredAmountRef);
+    ingredientInputRef.current.value = "";
+    amountInputRef.current.value = "";
+
+    // props.onAddIngredient(enteredIngredientName, enteredAmount);
+    // console.log(enteredIngredientName, enteredAmount);
+    // setEnteredIngrdientName("");
+    // setEnteredAmount("");
   };
 
-  const ingredientChangeHandler = (event) => {
-    setEnteredIngrdientName(event.target.value);
-  };
+  // const ingredientChangeHandler = (event) => {
+  //   setEnteredIngrdientName(event.target.value);
+  // };
 
-  const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
-  };
+  // const amountChangeHandler = (event) => {
+  //   setEnteredAmount(event.target.value);
+  // };
 
   const errorHandler = (event) => {
     setError(null);
@@ -70,8 +84,9 @@ const AddIngredient = (props) => {
           <input
             id="ingredient"
             type="text"
-            value={enteredIngrdientName}
-            onChange={ingredientChangeHandler}
+            // value={enteredIngredientName}
+            // onChange={ingredientChangeHandler}
+            ref={ingredientInputRef}
           />
           <label htmlFor="amount">
             얼만큼 넣을까요? 1:보통 / 2:조금 더 / 3:많이
@@ -79,8 +94,9 @@ const AddIngredient = (props) => {
           <input
             id="amount"
             type="text"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
+            // value={enteredAmount}
+            // onChange={amountChangeHandler}
+            ref={amountInputRef}
           />
           <Button type="submit">재료 추가</Button>
         </form>
