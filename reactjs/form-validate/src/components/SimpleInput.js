a99f2cba220c1e653c5f9be0607d131be53172c5
6lueparr0t <6lueparr0t@gmail.com>
2023-05-02 01:50:46 +0900

    add : react-query

--- reactjs/form-validate/src/components/SimpleInput.js
+++ reactjs/form-validate/src/components/SimpleInput.js
@@ -1,9 +1,29 @@
+import React, { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEneteredNames] = useState('');
+
+  const nameInputChangeHandler = event => {
+    setEneteredNames(event.target.value)
+  }
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
+    // setEnteredName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
