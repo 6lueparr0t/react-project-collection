afaaa3f3881708c1c8e06c2f6e714f03d2c28312
6lueparr0t <6lueparr0t@gmail.com>
2023-05-02 01:56:10 +0900

    update : form-validation

--- reactjs/form-validate/src/components/SimpleInput.js
+++ reactjs/form-validate/src/components/SimpleInput.js
@@ -2,13 +2,13 @@ import React, { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
-  const [enteredName, setEneteredNames] = useState('');
+  const [enteredName, setEnteredName] = useState("");
 
-  const nameInputChangeHandler = event => {
-    setEneteredNames(event.target.value)
-  }
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
 
-  const formSubmissionHandler = event => {
+  const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     console.log(enteredName);
@@ -16,14 +16,20 @@ const SimpleInput = (props) => {
     console.log(enteredValue);
 
     // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
-    // setEnteredName('');
-  }
+    setEnteredName("");
+  };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
