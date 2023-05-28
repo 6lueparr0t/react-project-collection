7504e5f3c674b9b56f3dbe1e77f2f5a7e6d718ae
6lueparr0t <6lueparr0t@gmail.com>
2023-05-28 19:42:35 +0900

    update : redux 마무리.

--- reactjs/redux/redux-demo-3-cart/src/store/ui-slice.js
+++ reactjs/redux/redux-demo-3-cart/src/store/ui-slice.js
@@ -1,15 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { cartIsVisible: false };
+const initialState = {
+  cartIsVisible: false,
+  notification: null,
+};
 
 const uiSlice = createSlice({
-  name: 'ui',
-  initialState : initialState,
-  reducers : {
-    toggle : (state) => {
+  name: "ui",
+  initialState: initialState,
+  reducers: {
+    toggle: (state) => {
       state.cartIsVisible = !state.cartIsVisible;
     },
-  }
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
 export const uiActions = uiSlice.actions;
