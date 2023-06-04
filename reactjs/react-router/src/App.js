3c35d7c0aa90fc5a8ae6f5cf0a6dee8626a99d67
6lueparr0t <6lueparr0t@gmail.com>
2023-06-04 18:56:56 +0900

    update : add router feature

--- reactjs/react-router/src/App.js
+++ reactjs/react-router/src/App.js
@@ -1,11 +1,37 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home';
-import ProductPage from './pages/Products';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route,
+} from "react-router-dom";
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+/**
+ * Component 와 JSX 를 사용한 방법
+ */
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage></HomePage>} />
+//     <Route path="/products" element={<ProductsPage></ProductsPage>} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage />,},
-  { path: '/products', element: <ProductPage />,},
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage/>,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
