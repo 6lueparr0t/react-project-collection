06000a3d2c1c9a3d69752173fdc13e351591284c
6lueparr0t <6lueparr0t@gmail.com>
2023-06-30 01:05:24 +0900

    update : react-router-adv, defer & Await & Suspense

--- reactjs/react-router-adv/frontend/src/pages/EventDetail.js
+++ reactjs/react-router-adv/frontend/src/pages/EventDetail.js
@@ -1,35 +1,66 @@
-import { useRouteLoaderData, json, redirect } from "react-router-dom";
+import { useRouteLoaderData, json, redirect, defer, Await } from "react-router-dom";
 import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
+  const {event, events} = useRouteLoaderData("event-detail");
 
-  // 에러 처리
-  if (data?.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  const event = data.event;
-
+  // 상세 페이지에서 페이지 내용과 리스트를 보여준다.
   return (
-    <EventItem event={event} />
+    <>
+      <Suspense fallback={<p style={({textAlign : "center"})}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={({textAlign : "center"})}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
   )
 }
 
 export default EventDetailPage;
 
-// 컴포넌트 마다 loader 를 두어, App.js 의 Router 에서 선언한다.
-export async function loader({request, params}) {
- const eventId = params.eventId;
- const response = await fetch(`http://localhost:8080/events/${eventId}`);
+const loadEvent = async (eventId) => {
+  const response = await fetch(`http://localhost:8080/events/${eventId}`);
  
  if(!response.ok) {
   return json({ message: "Could not fetch details for selected event." }, {
     status: 500,
   });
  } else {
-  return response;
+  const resData = await response.json();
+  return resData.event;
  }
+};
+
+const loadEvents = async () => {
+  const response = await fetch(`http://localhost:8080/events/`);
+ 
+ if(!response.ok) {
+  return json({ message: "Could not fetch details for selected event." }, {
+    status: 500,
+  });
+ } else {
+  const resData = await response.json();
+  return resData.events;
+ }
+};
+
+// 컴포넌트 마다 loader 를 두어, App.js 의 Router 에서 선언한다.
+export async function loader({request, params}) {
+ const eventId = params.eventId;
+ 
+ // loadEvent 앞에 await 를 주면 event 를 기다렸다가
+ // 그 다음 리스트를 출력한다.
+ return defer({
+  event: await loadEvent(eventId), 
+  events: loadEvents(),
+ });
 }
 
 // 삭제 event 정의
--- reactjs/react-router-adv/frontend/src/pages/Events.js
+++ reactjs/react-router-adv/frontend/src/pages/Events.js
@@ -1,31 +1,52 @@
-import { useLoaderData, json } from "react-router-dom";
+import { useLoaderData, json, defer, Await } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 const EventsPage = () => {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
+  return (
+    <Suspense fallback={<p style={({textAlign : "center"})}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 
-  const events = data.events;
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
 
-  return <EventsList events={events} />;
+  // const events = data.events;
+
+  // return <EventsList events={events} />;
 };
 
 export default EventsPage;
 
-export async function loader() {
+const loadEvents = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
     //   status: 500,
     // });
-    return json({ message: "Could not fetch events." }, {
-      status: 500,
-    });
+    return json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+};
+
+export function loader() {
+  // 데이터 fetching을 연기(deferring)
+  // 데이터가 도착할 때까지 기다리지 않고 페이지를 표시할 때 사용
+  return defer({
+    events: loadEvents(),
+  });
 }
