c81540ea98e987e3bb7d8250b8cac3a35e4a516c
6lueparr0t <6lueparr0t@gmail.com>
2023-06-30 11:31:06 +0900

    update : date

--- reactjs/expense-list/src/components/Expenses/ExpenseDate.js
+++ reactjs/expense-list/src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,16 @@
 import './ExpenseDate.scss'
 
+// 참고
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+
 const ExpenseDate = (props) => {
   const year = props.date.getFullYear();
-  const month = props.date.toLocaleString('ko-KR', {month: 'long'});
-  const day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
+  // const month = props.date.toLocaleString('ko-KR', {month: 'long'});
+  const month = props.date.toLocaleString('en-US', {month: '2-digit'});
+  // const month = props.date.getMonth()+1;
+  // const day = props.date.toLocaleString('ko-KR', {day: '2-digit'});
+  const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+  // const day = props.date.getDate();
 
   return (
     <div className="expense-date">
