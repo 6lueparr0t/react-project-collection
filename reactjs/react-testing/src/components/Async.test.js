040d964406e5efa3080cf26937bd3875aff6bc7d
6lueparr0t <6lueparr0t@gmail.com>
2023-08-08 15:41:52 +0900

    add : react-testing done

--- reactjs/react-testing/src/components/Async.test.js
+++ reactjs/react-testing/src/components/Async.test.js
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Async from "./Async";
 
-describe('Async component', () => {
-  test('renders posts if request succeeds', () => {
+describe("Async component", () => {
+  test("renders posts if request succeeds", async () => {
+    // fecth 함수를 mock 으로 만든다.
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: 'p1', title: 'First post'}]
+    });
     render(<Async />);
 
-    const listItemElements = screen.getAllByRole('listitem');
+    const listItemElements = await screen.findAllByRole("listitem", {}, {});
+    // console.log(listItemElements.length);
     expect(listItemElements).not.toHaveLength(0);
   });
-})
+});
