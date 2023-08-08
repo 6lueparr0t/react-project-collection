192c8388f8ad330fbc0c9caeb18b4d2353a6ad5d
6lueparr0t <6lueparr0t@gmail.com>
2023-08-08 17:57:20 +0900

    add : typescript

---
+++ reactjs/typescript/start/basics.ts
@@ -0,0 +1,17 @@
+// Primitives : number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = "Daehyun";
+
+let isInstructor: boolean;
+
+isInstructor = true;
---
+++ reactjs/typescript/start/no-typescript.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  return a + b;
+}
+
+const normal = add(2, 5);
+console.log(normal);
+
+const abnormal = add('2', '5');
+console.log(abnormal);
