0d913bbbf29bee3c71c51359b575a34557c74204
6lueparr0t <6lueparr0t@gmail.com>
2023-09-11 02:23:06 +0900

    04_guess-my-number : update screen

--- 05_react-native/04_guess-my-number/screens/StartGameScreen.tsx
+++ 05_react-native/04_guess-my-number/screens/StartGameScreen.tsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 
-import PrimaryButton from "../components/PrimaryButton";
+import PrimaryButton from '../components/PrimaryButton';
+import Colors from '../constants/Colors';
 
-function StartGameScreen() {
+const StartGameScreen: React.FC<{
+  onPickNumber: (pickedNumber: number) => void;
+}> = ({onPickNumber}) => {
   const [enteredNumber, setEnteredNumber] = useState<string>("");
 
   function numberInputHandler(enteredText: string): void {
     setEnteredNumber(enteredText);
   }
 
-  function confirmInputHandler() {
-    console.log("confirmInputHandler Pressed!");
+  function resetInputHandler() {
+    setEnteredNumber("");
+  }
+
+  function confirmInputHandler(): void {
+    const chosenNumber = parseInt(enteredNumber);
+
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        "Invalid number!",
+        "Number has to be a number between 1 and 99.",
+        [{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+      );
+      return;
+    }
+
+    // console.log("Valid number!");
+    onPickNumber(chosenNumber);
   }
 
   return (
@@ -35,21 +54,21 @@ function StartGameScreen() {
       </View>
     </View>
   );
-}
+};
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4,
-    shadowColor: "black",
+    shadowColor: 'black',
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
@@ -58,15 +77,15 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
-    fontWeight: "bold",
-    textAlign: "center",
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
   buttonsContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   buttonContainer: {
     flex: 1,
