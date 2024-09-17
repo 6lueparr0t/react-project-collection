e34d2f40aec01b97f682da2df6ff925034a35460
6lueparr0t <6lueparr0t@gmail.com>
2024-09-18 03:33:43 +0900

    feat: start, understanding-ts

---
+++ 03_typescript/01_understanding-ts/app.ts
@@ -0,0 +1,13 @@
+function add(n1: number, n2: number) {
+  // 이 코드는 런타임에 체크되지만, 우리는 컴파일 타임에 체크할 수 있다.
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //  throw new Error('Incorrect input!');
+  // }
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
