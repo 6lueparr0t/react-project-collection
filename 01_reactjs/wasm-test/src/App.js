765d462e9ed47d5e88226a3d79c6ab936ddf173f
6lueparr0t <6lueparr0t@gmail.com>
2024-11-04 09:18:53 +0900

    feat: update AsemblyScript

--- 01_reactjs/wasm-test/src/App.js
+++ 01_reactjs/wasm-test/src/App.js
@@ -1,45 +1,39 @@
-import { useEffect, useState } from 'react';
-import wasmModuleUrl from './assembly/module.wasm?url';
+import { useEffect, useState, useRef } from 'react';
+import wasmModuleUrl from './assembly/module.wasm';
 
 function App() {
   const [message, setMessage] = useState("");
 
-  let initialized = false;
+  const initialized = useRef(false);
 
   const getString = (memory, ptr) => {
     // Get the length of the string from memory
-    const length = new Uint16Array(memory.buffer)[ptr/8];
+    const length = new Uint16Array(memory.buffer)[ptr / 8];
 
     // Get the character codes from memory
     const codes = new Uint16Array(memory.buffer).subarray((ptr / 2), (ptr / 2) + length);
-  
+
     // Convert the character codes to a string
     return String.fromCharCode(...codes);
-    // // Decode them as UTF-8 and return the string
-    // return new TextDecoder("utf-8").decode(bytes);
   }
 
   useEffect(() => {
-    if (initialized) return () => {};
-    initialized = true;
+    if (initialized.current) return;
+    initialized.current = true;
 
     async function loadWasmModule() {
       const response = await fetch(wasmModuleUrl);
-      const buffer = await WebAssembly.compile(await response.arrayBuffer());
-      const importObject = {
+      const buffer = await response.arrayBuffer();
+      const module = await WebAssembly.instantiate(buffer, {
         env: {
           abort: () => console.log("Abort!"),
-          // memory: new WebAssembly.Memory({ initial: 1024 }),
         }
-      };
+      });
 
-      const module = await WebAssembly.instantiate(buffer, importObject);
-      return module;
+      return module.instance.exports;
     }
 
-    loadWasmModule().then((module) => {
-      const wasm = module.exports;
-
+    loadWasmModule().then((wasm) => {
       console.log(wasm.staticOne());
       console.log(wasm.staticAdd(1, 2));
       console.log(wasm.instanceTwo());
@@ -48,8 +42,6 @@ function App() {
       const helloWorld = getString(wasm.memory, wasm.helloWorld());
       setMessage(helloWorld);
     });
-
-    return () => {};
   }, []);
 
   return <h1>{message}</h1>;
