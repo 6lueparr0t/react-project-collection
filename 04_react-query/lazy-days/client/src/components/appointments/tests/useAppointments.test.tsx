f4546cb014b69e0b6321c08b040535575bf0375c
6lueparr0t <6lueparr0t@gmail.com>
2025-01-10 21:59:08 +0900

    feat: react-query, lazy-days react-query test 2 (mutation & custom hook)

--- 04_react-query/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
+++ 04_react-query/lazy-days/client/src/components/appointments/tests/useAppointments.test.tsx
@@ -1,10 +1,35 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
-
 import { useAppointments } from "../hooks/useAppointments";
 import { AppointmentDateMap } from "../types";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { act, renderHook, waitFor } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
+
+// a helper function to get the total number of appointments from an AppointmentDateMap
+const getAppointmentCount = (appointments: AppointmentDateMap) => {
+  return Object.values(appointments).reduce(
+    (runningCount, appoinmentsOnDate) => runningCount + appoinmentsOnDate.length,
+    0
+  );
+};
 
 test("filter appointments by availability", async () => {
-  // test goes here
+  const { result } = renderHook(() => useAppointments(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for appoinments to populate
+  await waitFor(() => expect(getAppointmentCount(result.current.appointments)).toBeGreaterThan(0));
+
+  // appoinments start out filtered )show only available)
+  const filteredAppoinmentsLength = getAppointmentCount(result.current.appointments);
+
+  // set to return all appointments
+  act(() => result.current.setShowAll(true));
+
+  // wait for count of appointments to be greater than when filtered
+  await waitFor(() =>
+    expect(getAppointmentCount(result.current.appointments)).toBeGreaterThan(
+      filteredAppoinmentsLength
+    )
+  );
 });
--- 04_react-query/lazy-days/client/src/components/staff/tests/useStaff.test.tsx
+++ 04_react-query/lazy-days/client/src/components/staff/tests/useStaff.test.tsx
@@ -1,9 +1,26 @@
-// import { act, renderHook, waitFor } from "@testing-library/react";
-
 import { useStaff } from "../hooks/useStaff";
 
-// import { createQueryClientWrapper } from "@/test-utils";
+import { act, renderHook, waitFor } from "@/test-utils";
+import { createQueryClientWrapper } from "@/test-utils";
 
 test("filter staff", async () => {
-  // the magic happens here
+  const { result } = renderHook(() => useStaff(), {
+    wrapper: createQueryClientWrapper(),
+  });
+
+  // wait for staff to populate
+  await waitFor(() => expect(result.current.staff).toHaveLength(4));
+
+  // set to filter for only staff who give massage
+  act(() => result.current.setFilter("facial"));
+
+  // wailt for staff list to display only 3
+  await waitFor(() => {
+    expect(result.current.staff).toHaveLength(3);
+  });
+
+  await waitFor(() => {
+    const filteredStaff = result.current.staff;
+    expect(filteredStaff.every((staff) => staff.treatmentNames.includes("facial"))).toBe(true);
+  });
 });
