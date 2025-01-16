02b5c4119f2a164bca63641e6aef7e24d8e84902
6lueparr0t <6lueparr0t@gmail.com>
2025-01-16 20:40:20 +0900

    test: start react test

--- 05_test/vite-starter/src/App.test.jsx
+++ 05_test/vite-starter/src/App.test.jsx
@@ -1,8 +1,79 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+// import { logRoles } from "@testing-library/dom";
 import App from "./App";
+import { kebabCaseToTitleCase } from "./helpers";
 
-test("App contains correct heading", () => {
+test("button has correct label and color after click", () => {
+  // const { container } = render(<App />);
+  // logRoles(container);
+
+  // render app
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+
+  // find the button
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+
+  // check the button
+  fireEvent.click(buttonElement);
+
+  // click the button text
+  expect(buttonElement).toHaveTextContent(/red/i);
+
+  // check the button color
+  expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+test("checkbox flow", () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable button/i });
+
+  // check initial conditions
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("medium-violet-red");
+});
+
+test("checkbox flow", () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable button/i });
+
+  // click checkbox to disable button
+  fireEvent.click(buttonElement);
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("midnight-blue");
+});
+
+describe("kebabCaseToTitleCase", () => {
+  test("works for no hyphen", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+  test("works for no hyphen", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+  test("works for no hyphen", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
---
+++ 05_test/vite-starter/src/helpers.js
@@ -0,0 +1,6 @@
+export function kebabCaseToTitleCase(colorName) {
+  const colorWithSpaces = colorName.replace(/-/g, " ");
+  const colorWithCaps = colorWithSpaces.replace(/\b([a-z])/g, (match) => match.toUpperCase());
+
+  return colorWithCaps;
+}
